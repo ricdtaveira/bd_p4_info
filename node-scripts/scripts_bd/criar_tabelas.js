f58517696db9914cbe1975cc5a3bc0600b4ecf7a
Ricardo Taveira <ricdtaveira@gmail.com>
2021-10-27 14:59:58 -0300

    Alterado em 27-10-2021

--- node-scripts/scripts_bd/criar_tabelas.js
+++ node-scripts/scripts_bd/criar_tabelas.js
@@ -5,23 +5,25 @@ const db = new sqlite3.Database('./node-scripts/database/DB_PESSOAL_RH.db', (err
 });
 
 db.serialize(() => {
-    db.run('create table if not exists people (id integer primary key, name text)', (error) => {
+    db.run('create table if not exists TB_PESSOAS (id integer primary key autoincrement , name text)', (error) => {
         if (error) console.log(error);
     });
 
-    db.run('insert into people (name) values (?)', ['João Alves Damasceno'], (error) => {
+    db.run('insert into TB_PESSOAS (name) values (?)', ['João Alves Damasceno'], (error) => {
         if (error) console.log(error);
     });
 
-    db.run('insert into people (name) values (?)', ['José Nascimento Silva'], (error) => {
+    db.run('insert into TB_PESSOAS (name) values (?)', ['José Nascimento Silva'], (error) => {
         if (error) console.log(error);
     });
 
-    db.run('insert into people (name) values (?)', ['Maria Antonieta Braga'], (error) => {
+    db.run('insert into TB_PESSOAS (name) values (?)', ['Maria Antonieta Braga'], (error) => {
         if (error) console.log(error);
     });
+    
+    console.log("TB_PESSOAS")
 
-    db.all('select * from people', (error, rows) => {
+    db.all('select * from TB_PESSOAS', (error, rows) => {
         console.log(error, rows);
     });
 });
